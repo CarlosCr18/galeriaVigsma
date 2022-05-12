58cc37ae0f32cf05eff6bd49794c1784c54d1ab0
CarlosCr18 <carlosLcrespo18@gmail.com>
2022-05-12 18:12:01 -0500

    galerias creadas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import Carousel from "react-gallery-carousel";
+import "./App.css";
+import "react-gallery-carousel/dist/index.css";
 
 function App() {
+  function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => {
+      return (images[item.replace("./", "")] = r(item));
+    });
+    return images;
+  }
+  const bovedillas = importAll(
+    require.context("./bovedillas", false, /\.(png|jpe?g|svg|webp)$/)
+  );
+
+  let bovedillasSrcValue = [];
+  Object.values(bovedillas).forEach((item, index) => {
+    bovedillasSrcValue.push({
+      src: item,
+    });
+  });
+  const obras = importAll(
+    require.context("./obras", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let obrasSrcValue = [];
+  Object.values(obras).forEach((item, index) => {
+    obrasSrcValue.push({
+      src: item,
+      sizes: `100px`,
+    });
+  });
+  const viguetas = importAll(
+    require.context("./viguetas", false, /\.(png|jpe?g|svg)$/)
+  );
+
+  let viguetasSrcValue = [];
+  Object.values(viguetas).forEach((item, index) => {
+    viguetasSrcValue.push({
+      src: item,
+      sizes: `100px`,
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="titulo">Galeria vigsma</h1>
+      <div className="App">
+        <div className="contenedorCarousel offsetIzquierdo">
+          <h2 className="tituloDeCarousel ">Obras</h2>
+          <Carousel
+            images={obrasSrcValue}
+            isMaximized={true}
+            isAutoPlaying={true}
+            autoPlayInterval={3500}
+            transitionDurationLimit={1000}
+            canAutoPlay={true}
+          />
+        </div>
+        <div className="contenedorCarousel">
+          <h2 className="tituloDeCarousel">Bovedillas</h2>
+          <Carousel
+            images={bovedillasSrcValue}
+            isAutoPlaying={false}
+            transitionDurationLimit={1000}
+            canAutoPlay={false}
+          />
+        </div>
+        <div className="contenedorCarousel offsetDerecho">
+          <h2 className="tituloDeCarousel">Viguetas</h2>
+          <Carousel
+            images={viguetasSrcValue}
+            transitionDurationLimit={1000}
+            isAutoPlaying={false}
+            canAutoPlay={false}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
